c1c1de377f4f2692242c42ab850303e224b3aa4a
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2023-08-10 10:46:02 +0530

    Create SumOfArray.java
    Time Complexity: O(N)
    Auxiliary Space: O(1)

---
+++ SumOfArray.java
@@ -0,0 +1,10 @@
+class Solution {
+    int sum(int arr[], int n) {
+        // code here
+        int totalSum =0;
+        for(int i: arr){
+            totalSum +=i;
+        }
+        return totalSum;
+    }
+}
