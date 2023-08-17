31a742eea0403d801f447919df374fc5aee420e0
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2023-08-17 11:23:27 +0530

    Create JavaArraysSet1.java
    Input:
    5
    1 2 3 4 5
    Output:
    15 3.00 
    Explanation:
    Sum of the array is 15, hence 
    average is 15/5=3.00.

---
+++ JavaArraysSet1.java
@@ -0,0 +1,14 @@
+
+class Compute
+{
+    String average(int A[], int N)
+    {
+        float sum =0;
+        for(int i =0; i<N; i++)
+        {
+            sum+= A[i];
+        }
+        
+        return String.format("%.2f", sum/N);
+    }
+}
