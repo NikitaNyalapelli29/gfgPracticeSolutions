aca729e8bc44bb0e2dabbd174cc7c1560b093d5b
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2023-08-17 11:01:15 +0530

    Create PerfectArray.java
    Input : Arr[] = {1, 2, 3, 2, 1}
    Output : PERFECT
    Explanation:
    Here we can see we have [1, 2, 3, 2, 1] 
    if we reverse it we can find [1, 2, 3, 2, 1]
    which is the same as before.
    So, the answer is PERFECT.

---
+++ PerfectArray.java
@@ -0,0 +1,15 @@
+class Solution{
+    
+    public boolean IsPerfect(int a[], int n)
+    {
+        // Complete the function
+        for(int i =0; i<n/2; i++){
+            if(a[i] != a[n-i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
+
